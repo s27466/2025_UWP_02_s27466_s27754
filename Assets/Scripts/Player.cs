54e969faad883176b2aad3b0dec5a412572e10f9
s27466 <161813484+s27466@users.noreply.github.com>
2025-03-14 18:21:43 +0100

    szkielet zadania

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _MaxHP;
+    [SerializeField] private float _HP;
+
+}
